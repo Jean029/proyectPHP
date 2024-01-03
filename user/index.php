966191738d4498c6ce9c8b987595247fcb1559d1
Mary0711 <mariliana.barrientos@upr.edu>
2024-01-02 22:27:44 -0400

    admin html

--- user/index.php
+++ user/index.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-
+/*
 if (!isset($_SESSION['username'])) {
     header("Location: ../index.php");
-}
+}*/
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,7 +16,8 @@ if (!isset($_SESSION['username'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet" />
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <link href="../css/user.css" rel="stylesheet" />
     <script src="../js/user.js"></script>
 </head>
